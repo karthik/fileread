a80c0e13e8102e490f757faf7baf2fb0167be8da
Karthik Ram <karthik.ram@gmail.com>
2015-05-27 19:23:14 -0400

    Cleaned up file

--- readfiles.R
+++ readfiles.R
@@ -5,11 +5,17 @@ df <- data.frame(a = 1:3,
 				 fname = c("f1.txt", "f2.txt", "f3.txt"),
 				 stringsAsFactors = FALSE)
 
+# > df
+#   a b c  fname
+# 1 1 4 7 f1.txt
+# 2 2 5 8 f2.txt
+# 3 3 6 9 f3.txt
 
 # This function reads a file from a filename
 read_file <- function(fname) {
-	paste(readLines(fname), collapse = " ")
+	readChar(fname, file.info(fname)$size)
 }
 
 library(dplyr)
-df %>% mutate(cc = read_file(fname))
+df %>% 
+	mutate(cc = read_file(.$fname))
